af11883b8e90947748edf82c2424f99bb2d01e65
Omid Shahbazi <sh.omid.m@gmail.com>
2017-05-08 23:46:05 +0430

    Minor change

--- SourceCode/Engine/Engine/Platform/PlatformNetwork.cpp
+++ SourceCode/Engine/Engine/Platform/PlatformNetwork.cpp
@@ -1,5 +1,6 @@
 // Copyright 2016-2017 ?????????????. All Rights Reserved.
 #include <Platform\PlatformNetwork.h>
+#include <Common\BitwiseUtils.h>
 #include <winsock2.h>
 
 #pragma comment(lib, "wsock32.lib")
@@ -53,6 +54,28 @@ namespace Engine
 			return IPPROTO_MAX;
 		}
 
+		int GetSendFlags(PlatformNetwork::SendModes Mode)
+		{
+			int flags = 0;
+
+			if (BitwiseUtils::IsEnabled(Mode, PlatformNetwork::SendModes::DontRoute))
+				flags |= MSG_DONTROUTE;
+
+			if (BitwiseUtils::IsEnabled(Mode, PlatformNetwork::SendModes::OutOfBand))
+				flags |= MSG_OOB;
+
+			return IPPROTO_MAX;
+		}
+
+		sockaddr_in GetAddressInfo(PlatformNetwork::AddressFamilies AddressFamily, uint16 Port)
+		{
+			sockaddr_in address;
+			address.sin_family = GetAddressFamiliy(AddressFamily);
+			address.sin_addr.S_un.S_addr = INADDR_ANY;
+			address.sin_port = Port;
+			return address;
+		}
+
 		bool PlatformNetwork::Initialize(void)
 		{
 			WSADATA data;
@@ -76,12 +99,23 @@ namespace Engine
 
 		bool PlatformNetwork::Bind(Handle Handle, AddressFamilies AddressFamily, uint16 Port)
 		{
-			sockaddr_in address;
-			address.sin_family = GetAddressFamiliy(AddressFamily);
-			address.sin_addr.S_un.S_addr = INADDR_ANY;
-			address.sin_port = Port;
+			sockaddr_in &&address = GetAddressInfo(AddressFamily, Port);
 
 			return (bind(Handle, reinterpret_cast<const sockaddr*>(&address), sizeof(sockaddr_in)) >= 0);
+		}
+
+		bool PlatformNetwork::SetNonBlocking(Handle Handle, bool Enabled)
+		{
+			DWORD enabled = (Enabled ? 1 : 0);
+
+			return (ioctlsocket(Handle, FIONBIO, &enabled) == NO_ERROR);
+		}
+
+		bool PlatformNetwork::Send(Handle Handle, const byte *Buffer, uint32 Length, AddressFamilies AddressFamily, uint16 Port, SendModes Mode)
+		{
+			sockaddr_in &&address = GetAddressInfo(AddressFamily, Port);
+			WSANOTINITIALISED
+			return (sendto(Handle, reinterpret_cast<const char*>(Buffer), Length, GetSendFlags(Mode), reinterpret_cast<const sockaddr*>(&address), sizeof(sockaddr_in)) == Length);
 		}
 	}
 }
--- SourceCode/Engine/Engine/Test/TestNetwork/TestNetwork.cpp
+++ SourceCode/Engine/Engine/Test/TestNetwork/TestNetwork.cpp
@@ -1,6 +1,7 @@
 ﻿
 #include <Platform\PlatformNetwork.h>
 
+using namespace Engine::Common;
 using namespace Engine::Platform;
 
 void main()
@@ -12,6 +13,14 @@ void main()
 
 
 	if (!PlatformNetwork::Bind(handle, PlatformNetwork::AddressFamilies::InterNetwork, 30001))
+		return;
+
+	if (!PlatformNetwork::SetNonBlocking(handle, true))
+		return;
+
+	const char * buffer = "omid";
+
+	if (!PlatformNetwork::Send(handle, reinterpret_cast<const byte*>(buffer), 5, PlatformNetwork::AddressFamilies::InterNetwork, 30001, PlatformNetwork::SendModes::None))
 		return;
 
 	PlatformNetwork::Close(handle);

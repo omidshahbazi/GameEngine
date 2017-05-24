6a0e86ede5a4047884b04da371c76f14803c9235
Omid Shahbazi <sh.omid.m@gmail.com>
2017-05-24 11:35:48 +0000

    Minor change

--- SourceCode/Engine/Frontend/Program.cs
+++ SourceCode/Engine/Frontend/Program.cs
@@ -8,6 +8,16 @@ namespace Engine.Frontend
 {
 	class Program
 	{
+		private static bool IsEnumDefine<T>(string Name)
+		{
+			return Enum.IsDefined(typeof(T), Name);
+		}
+
+		private static T GetEnum<T>(string Name)
+		{
+			return (T)Enum.Parse(typeof(T), Name);
+		}
+
 		static int Main(string[] Args)
 		{
 			//FilesMetaDataGenerator.Generate(@"E:\Omid\New folder\SourceCode");
@@ -15,21 +25,21 @@ namespace Engine.Frontend
 			Args = new string[] { "-Engine", "-X86", "-Debug" };
 			//Args = new string[] { "-ProjectFile" };
 
-			if (Enum.IsDefined(typeof(BuildSystem.TargetsToBuild), Args[0].Substring(1)))
+			if (IsEnumDefine<BuildSystem.TargetsToBuild>(Args[0].Substring(1)))
 			{
-				BuildSystem.TargetsToBuild toBuild = (BuildSystem.TargetsToBuild)Enum.Parse(typeof(BuildSystem.TargetsToBuild), Args[0].Substring(1));
+				BuildSystem.TargetsToBuild toBuild = GetEnum<BuildSystem.TargetsToBuild>(Args[0].Substring(1));
 
 				if (toBuild == BuildSystem.TargetsToBuild.ProjectFile)
 				{
 					EngineProjectFileCreator.Create();
 				}
-				else if (Args.Length >= 3 && Enum.IsDefined(typeof(BuildSystem.PlatformArchitectures), Args[1].Substring(1)))
+				else if (Args.Length >= 3 && IsEnumDefine<BuildSystem.PlatformArchitectures>(Args[1].Substring(1)))
 				{
-					BuildSystem.PlatformArchitectures architecture = (BuildSystem.PlatformArchitectures)Enum.Parse(typeof(BuildSystem.PlatformArchitectures), Args[1].Substring(1));
+					BuildSystem.PlatformArchitectures architecture = GetEnum<BuildSystem.PlatformArchitectures>(Args[1].Substring(1));
 
-					if (Enum.IsDefined(typeof(ProjectFileGenerator.BuildConfigurations), Args[2].Substring(1)))
+					if (IsEnumDefine<ProjectFileGenerator.BuildConfigurations>(Args[2].Substring(1)))
 					{
-						ProjectFileGenerator.BuildConfigurations buildConfiguration = (ProjectFileGenerator.BuildConfigurations)Enum.Parse(typeof(ProjectFileGenerator.BuildConfigurations), Args[2].Substring(1));
+						ProjectFileGenerator.BuildConfigurations buildConfiguration = GetEnum<ProjectFileGenerator.BuildConfigurations>(Args[2].Substring(1));
 
 						BuildSystem builder = new BuildSystem(toBuild, architecture, buildConfiguration);
 						if (builder.Build())

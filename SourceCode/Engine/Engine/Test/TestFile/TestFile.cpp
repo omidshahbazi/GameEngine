f44152ab7abca73de571b48ab2c05de61a73531a
Omid Shahbazi <sh.omid.m@gmail.com>
2017-05-24 11:13:03 +0000

    Minor change

--- SourceCode/Engine/Engine/Test/TestFile/TestFile.cpp
+++ SourceCode/Engine/Engine/Test/TestFile/TestFile.cpp
@@ -1,20 +1,20 @@
-﻿//#include <Platform\PlatformFile.h>
-//#include <Common\BitwiseUtils.h>
-//
-//using namespace Engine::Platform;
-//using namespace Engine::Common;
-//
-//void main()
-//{
-//	PlatformFile::Handle handle = PlatformFile::Open("E:/1.txt", PlatformFile::Modes::Output | PlatformFile::Modes::WideCharacter);
-//
-//
-//	PlatformFile::Write(handle, 10);
-//	PlatformFile::Write(handle, "aasdasd");
-//	PlatformFile::Write(handle, L'س');
-//
-//
-//
-//
-//	PlatformFile::Close(handle);
-//}
+﻿#include <Platform\PlatformFile.h>
+#include <Common\BitwiseUtils.h>
+
+using namespace Engine::Platform;
+using namespace Engine::Common;
+
+void main()
+{
+	PlatformFile::Handle handle = PlatformFile::Open("E:/1.txt", PlatformFile::Modes::Output | PlatformFile::Modes::WideCharacter);
+
+
+	PlatformFile::Write(handle, 10);
+	PlatformFile::Write(handle, "aasdasd");
+	PlatformFile::Write(handle, L'س');
+
+
+
+
+	PlatformFile::Close(handle);
+}
--- SourceCode/Engine/Engine/Test/TestParallelizing/TestParallelizing.cpp
+++ SourceCode/Engine/Engine/Test/TestParallelizing/TestParallelizing.cpp
@@ -1,32 +1,32 @@
-//#include <MemoryManagement\SharedMemory.h>
-//#include <MemoryManagement\Allocator\DefaultAllocator.h>
-//#include <MemoryManagement\Allocator\FixedSizeAllocator.h>
-//#include <MemoryManagement\Allocator\DynamicSizeAllocator.h>
-//#include <Parallelizing\JobManager.h>
-//#include <Common\PrimitiveTypes.h>
-//#include <Platform\PlatformThread.h>
-//#include <Containers\DynamicString.h>
-//#include <chrono>
-//#include <iostream>
-//
-//#include <vector>
-//
-//using namespace Engine::MemoryManagement;
-//using namespace Engine::Common;
-//using namespace Engine::MemoryManagement::Allocator;
-//using namespace Engine::Parallelizing;
-//
-//int Add(int a, int b)
-//{
-//	return a + b;
-//}
-//
-//void main()
-//{
-//	RunJob(Add, 1, 2);
-//
-//	while (true)
-//	{
-//		Engine::Platform::PlatformThread::Sleep(1000);
-//	}
-//}
+#include <MemoryManagement\SharedMemory.h>
+#include <MemoryManagement\Allocator\DefaultAllocator.h>
+#include <MemoryManagement\Allocator\FixedSizeAllocator.h>
+#include <MemoryManagement\Allocator\DynamicSizeAllocator.h>
+#include <Parallelizing\JobManager.h>
+#include <Common\PrimitiveTypes.h>
+#include <Platform\PlatformThread.h>
+#include <Containers\DynamicString.h>
+#include <chrono>
+#include <iostream>
+
+#include <vector>
+
+using namespace Engine::MemoryManagement;
+using namespace Engine::Common;
+using namespace Engine::MemoryManagement::Allocator;
+using namespace Engine::Parallelizing;
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+
+void main()
+{
+	RunJob(Add, 1, 2);
+
+	while (true)
+	{
+		Engine::Platform::PlatformThread::Sleep(1000);
+	}
+}

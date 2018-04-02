24c55b776aeaaac08eb83185a1c0ecd9e847664b
Omid Shahbazi <sh.omid.m@gmail.com>
2018-04-02 12:02:34 +0430

    Minor change

--- SourceCode/Engine/Engine/Test/TestParallelizing/TestParallelizing.cpp
+++ SourceCode/Engine/Engine/Test/TestParallelizing/TestParallelizing.cpp
@@ -42,7 +42,7 @@ extern template class MEMORYMANAGEMENT_API SharedMemory<Job<int>>;
 
 int NewAdd()
 {
-	Job<int> desc1 = RunJob(Add, 1, 2);
+	Job<int> desc1 = RunJob([](int a, int b) { return Add(a, b); }, 1, 2);
 	Job<int> desc2 = RunJob(Value2);
 
 	while (!desc1.IsFinished() || !desc2.IsFinished())
@@ -56,9 +56,9 @@ int NewAdd()
 	return result;
 }
 
-void Do()
+void ReadFile(cstr Path)
 {
-	PlatformFile::Handle handle = PlatformFile::Open("E:/To Backup/Khane Be Dosh E15 (www.Downloadha.com).rar", PlatformFile::OpenModes::Binary | PlatformFile::OpenModes::Input);
+	PlatformFile::Handle handle = PlatformFile::Open(Path, PlatformFile::OpenModes::Binary | PlatformFile::OpenModes::Input);
 
 	uint64 size = PlatformFile::Size(handle);
 
@@ -66,40 +66,20 @@ void Do()
 
 	PlatformFile::Read(handle, buffer, size);
 
+	std::cout << Path;
 }
-
-struct aaa
-{
-	int b;
-};
 
 void main()
 {
 	JobManager::Create(DefaultAllocator::GetInstance());
-	//  make dependency for jobs
 	//	make waiting mechanism
 
-	Job<int> a = RunJob(Priority::High, Value2);// ->Then(Do);
-
-	//Job<int> bbb(a);
-
-	Job<int> b = RunJob(Value2);
-
+	Job<int> a = RunJob(NewAdd);
 
 	while (!a.IsFinished())
 	{
 		Engine::Platform::PlatformThread::Sleep(1000);
 	}
-
-	std::cout << a.Get();
-
-
-	while (!b.IsFinished())
-	{
-		Engine::Platform::PlatformThread::Sleep(1000);
-	}
-
-	std::cout << "file read finished";
 
 	JobManager::Destroy();
 }

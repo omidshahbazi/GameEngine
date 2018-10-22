b93d6d7c0bb8b8a88c6acbaded3184e0bd192afd
Omid Shahbazi <sh.omid.m@gmail.com>
2018-10-23 00:18:23 +0330

    Minor change

--- SourceCode/Engine/Engine/Rendering/Private/ShaderCompiler/Compiler.cpp
+++ SourceCode/Engine/Engine/Rendering/Private/ShaderCompiler/Compiler.cpp
@@ -3,21 +3,72 @@
 #include <Rendering\Private\ShaderCompiler\ShaderParser.h>
 #include <Rendering\Private\ShaderCompiler\StructType.h>
 #include <Rendering\Private\ShaderCompiler\FunctionType.h>
+#include <Containers\Strings.h>
+#include <Containers\StringUtility.h>
+#include <Containers\Map.h>
 
 namespace Engine
 {
+	using namespace Containers;
+
 	namespace Rendering
 	{
 		namespace Private
 		{
 			namespace ShaderCompiler
 			{
+				String GetOpenGLTypeByDataType(DataTypes Type)
+				{
+					switch (Type)
+					{
+					case DataTypes::Float:
+						return "float";
+
+					case DataTypes::Float2:
+						return "vec2";
+
+					case DataTypes::Float3:
+						return "vec3";
+
+					case DataTypes::Float4:
+						return "vec4";
+
+					case DataTypes::Float4X4:
+						return "mat4";
+					}
+					
+					return "";
+				}
+
 				void BuildOpenGLVertexShader(ShaderParser::StructTypeList StructList, ShaderParser::FunctionTypeList FunctionList, String &VertextShader)
 				{
+					Map<ConstString, uint16> layouts;
+					layouts["POSITION"] = 0;
+					layouts["NORMAL"] = 1;
+					layouts["TEXCOORD"] = 2;
+
 					VertextShader += "#version 330 core";
 
 					for each (auto st in StructList)
-						VertextShader += "\n" + st->ToString();
+					{
+						uint16 index = 0;
+						for each (auto var in st->GetVariables())
+						{
+							if (var->GetRegister().GetLength() == 0)
+								VertextShader += "uniform ";
+							else
+							{
+								VertextShader += "layout(location=";
+								VertextShader += StringUtility::ToString(index++) + ")";
+							}
+
+							VertextShader += GetOpenGLTypeByDataType(var->GetDataType());
+
+							VertextShader += var->GetName();
+
+							VertextShader += ";";
+						}
+					}
 
 					for each (auto fn in FunctionList)
 					{

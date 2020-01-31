921a288c763772348ccfd2e965fdc1ba6c0135c9
Omid Shahbazi <sh.omid.m@gmail.com>
2020-01-31 18:25:14 +0330

    Added      SourceCode/Engine/Editor/App.config Added      SourceCode/Engine/Editor/Editor.csproj Added      SourceCode/Engine/Editor/Program.cs Modified   SourceCode/Engine/Engine.sln Add        SourceCode/Engine/Editor/MainForm.Designer.cs Add        SourceCode/Engine/Editor/MainForm.cs Add        SourceCode/Engine/Editor/MainForm.resx Add        SourceCode/Engine/Editor/Properties/AssemblyInfo.cs

---
+++ SourceCode/Engine/Editor/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Containers;
+using Engine.CoreSystem;
+using System.Windows.Forms;
+
+namespace Engine.Editor
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+
+
+			Core core = Core.Instance;
+			var window =  core.CreateWindow(new Vector2I() { X = 800, Y = 600 }, "Editor");
+			core.Initialize();
+		}
+	}
+}

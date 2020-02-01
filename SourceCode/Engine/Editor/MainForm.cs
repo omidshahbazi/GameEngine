4f1809a8a56c49396ee69644bf0a0d75bf3f10f1
Omid Shahbazi <sh.omid.m@gmail.com>
2020-02-01 17:38:27 +0330

    Modified   SourceCode/Engine/Editor/MainForm.cs

--- SourceCode/Engine/Editor/MainForm.cs
+++ SourceCode/Engine/Editor/MainForm.cs
@@ -8,13 +8,26 @@ namespace Engine.Editor
 	//https://docs.microsoft.com/en-us/dotnet/framework/interop/marshaling-classes-structures-and-unions
 	public partial class MainForm : Form
 	{
+		private Core core = null;
+		private Timer timer = null;
+
 		public MainForm()
 		{
 			InitializeComponent();
 
-			Core core = Core.Instance;
-			var window =  core.CreateWindow(new Vector2I() { X = 800, Y = 600 }, "Editor");
+			core = Core.Instance;
+			var window = core.CreateWindow(new Vector2I() { X = 800, Y = 600 }, "Editor");
 			core.Initialize();
+
+			timer = new Timer();
+			timer.Interval = 1;
+			timer.Tick += Timer_Tick;
+			timer.Start();
+		}
+
+		private void Timer_Tick(object sender, System.EventArgs e)
+		{
+			core.Update();
 		}
 	}
 }

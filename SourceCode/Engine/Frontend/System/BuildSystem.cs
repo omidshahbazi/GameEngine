83d236d0f3d846a5f1453dd289e2fcbc1be57729
Omid Shahbazi <sh.omid.m@gmail.com>
2021-08-09 12:43:17 +0430

    Modified   SourceCode/Engine/Binaries/ASCIISerializerManaged.dll Modified   SourceCode/Engine/Binaries/Frontend.exe Modified   SourceCode/Engine/Frontend/System/BuildSystem.cs

--- SourceCode/Engine/Frontend/System/BuildSystem.cs
+++ SourceCode/Engine/Frontend/System/BuildSystem.cs
@@ -201,7 +201,7 @@ namespace Engine.Frontend.System
 					if (!BuildEngineBuilder(builder))
 						return false;
 
-					if (builder.State != EngineBuilder.States.Built)
+					if (builder.State == EngineBuilder.States.NotBuilt)
 						forceToRebuild = true;
 				}
 

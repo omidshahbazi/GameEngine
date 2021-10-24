d199ef27a9b96e5c905d791acc58c084500132fb
Omid Shahbazi <sh.omid.m@gmail.com>
2021-10-24 19:56:15 +0330

    Modified   Engine/Source/Runtime/ThirdParty/GLSLANG/GLSLANG.ModuleRules.cs Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/Win32/GenericCodeGend.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/Win32/MachineIndependentd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/Win32/OGLCompilerd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/Win32/OSDependentd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/Win32/SPIRVd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/Win32/glslangd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/x64/GenericCodeGend.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/x64/MachineIndependentd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/x64/OGLCompilerd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/x64/OSDependentd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/x64/SPIRVd.lib Added      Engine/Source/Runtime/ThirdParty/GLSLANG/lib/Debug/x64/glslangd.lib

--- Engine/Source/Runtime/ThirdParty/GLSLANG/GLSLANG.ModuleRules.cs
+++ Engine/Source/Runtime/ThirdParty/GLSLANG/GLSLANG.ModuleRules.cs
@@ -3,8 +3,9 @@ namespace Engine.Runtime.ThirdParty
 {
 	// Update notes
 	// 1. Pull from https://github.com/KhronosGroup/glslang
-	// 2. Build it using instructions in the landing page
-	// 3. Update using built files
+	// 2. Remove extra item checks in CMake
+	// 3. Build
+	// 4. Update using built files
 
 	class GLSLANGModuleRules : ModuleRules
 	{
@@ -31,15 +32,29 @@ namespace Engine.Runtime.ThirdParty
 			}
 
 #if WIN32
+#if DEBUG_MODE
 			public override string[] LibraryPaths
 			{
-				get { return new string[] { "lib/Release/Win32/glslangd.lib", "lib/Release/Win32/OGLCompilerd.lib", "lib/Release/Win32/OSDependentd.lib", "lib/Release/Win32/SPIRVd.lib", "lib/Release/Win32/MachineIndependentd.lib", "lib/Release/Win32/GenericCodeGend.lib" }; }
+				get { return new string[] { "lib/Debug/Win32/glslangd.lib", "lib/Debug/Win32/OGLCompilerd.lib", "lib/Debug/Win32/OSDependentd.lib", "lib/Debug/Win32/SPIRVd.lib", "lib/Debug/Win32/MachineIndependentd.lib", "lib/Debug/Win32/GenericCodeGend.lib" }; }
 			}
+#elif RELEASE_MODE
+			public override string[] LibraryPaths
+			{
+				get { return new string[] { "lib/Release/Win32/glslang.lib", "lib/Release/Win32/OGLCompiler.lib", "lib/Release/Win32/OSDependent.lib", "lib/Release/Win32/SPIRV.lib", "lib/Debug/Win32/MachineIndependent.lib", "lib/Release/Win32/GenericCodeGen.lib" }; }
+			}
+#endif
 #elif X64
+#if DEBUG_MODE
+			public override string[] LibraryPaths
+			{
+				get { return new string[] { "lib/Debug/x64/glslangd.lib", "lib/Debug/x64/OGLCompilerd.lib", "lib/Debug/x64/OSDependentd.lib", "lib/Debug/x64/SPIRVd.lib", "lib/Debug/x64/MachineIndependentd.lib", "lib/Debug/x64/GenericCodeGend.lib" }; }
+			}
+#elif RELEASE_MODE
 			public override string[] LibraryPaths
 			{
 				get { return new string[] { "lib/Release/x64/glslang.lib", "lib/Release/x64/OGLCompiler.lib", "lib/Release/x64/OSDependent.lib", "lib/Release/x64/SPIRV.lib", "lib/Release/x64/MachineIndependent.lib", "lib/Release/x64/GenericCodeGen.lib" }; }
 			}
+#endif
 #endif
 		}
 	}
